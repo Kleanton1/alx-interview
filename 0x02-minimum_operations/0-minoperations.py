27ffb5b499a80c6c72d8f553225dd3c8f8d88e6b
Kleanton1 <ifeanyiemmanuel271@gmail.com>
2023-02-12 17:35:24 +0000

    minoperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 ''' Minimum Operations '''
 
+
 def minOperations(n):
- if not isinstance(n, int):
+    """
+    calculates fewest number of operations needed
+    to result in exactly n H characters in the file.
+    :param n: an integer
+    :return: Returns an integer
+    """
+    if not isinstance(n, int) or n <= 1:
         return 0
 
-    op = 0
+    factors = []
     i = 2
-    while (i <= n):
-        if not (n % i):
-            n = int(n / i)
-            op += i
-            i = 1
-        i += 1
-    return op
+    while i <= n:
+        if n % i == 0:
+            factors.append(i)
+            n = n / i
+        else:
+            i += 1
+    return sum(factors)
